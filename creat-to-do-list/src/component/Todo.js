bab840b631f709a06cfbebdfd1ae382fea286236
Elham Poshtiban <elham@londr.com>
2021-11-15 02:03:38 -0600

    #what try to compelet todo file

--- creat-to-do-list/src/component/Todo.js
+++ creat-to-do-list/src/component/Todo.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
 
 function Todo() {
   const [edit, setEdit] = useState({
     id: null,
     value: "",
   });
-  return <div></div>;
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isCompelet ? "todo-row compelet" : "todo-row"}
+      key={index}
+    ></div>
+  ));
 }
 
 export default Todo;
